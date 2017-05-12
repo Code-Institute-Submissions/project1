1040bc991966b84c4d0f079c833e3b98e8d4ca4e
David Watson <dave.watson8001@gmail.com>
2017-05-12 17:46:09 +0100

    form

---
+++ js/controller.js
@@ -0,0 +1,21 @@
+angular.module('RouteControllers', [])
+	.controller('BookingController', function($scope){
+
+		$scope.clientBooking = {};
+
+		$scope.submitForm = function(){
+			
+		if ($scope.bookingForm.$valid){
+			$scope.clientBooking.fname = $scope.client.fname;
+			$scope.clientBooking.sname = $scope.client.sname;
+			$scope.clientBooking.email = $scope.client.email;
+			$scope.clientBooking.phonenum = $scope.client.phonenum;
+			//$scope.clientBooking.bookingdate = $scope.client.bookingdate;
+			alert('Information submitted sucessfully!');
+		}
+
+		console.log($scope.clientBooking.fname + " " + $scope.clientBooking.sname + " " + 
+						$scope.clientBooking.email + " " + $scope.clientBooking.phonenum + " " + 
+						$scope.clientBooking.bookingdate);
+		};
+	});
